a0062b9cc01b06d92efffb6e4416aadc9d2bf61c
nmuelle2 <nina-marie.mueller@axelspringer.de>
2015-04-28 16:57:18 +0200

    initial commit

---
+++ src/test/java/GreetingControllerTest.java
@@ -0,0 +1,38 @@
+/*
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MockServletContext.class)
+@WebAppConfiguration
+public class GreetingControllerTest {
+
+    private MockMvc mvc;
+    private String greetingString = "Hallo Welt!!!";
+
+    @Before
+    public void setUp() throws Exception{
+        mvc = MockMvcBuilders.standaloneSetup(new GreetingController()).build();
+    }
+
+    @Test
+    public void greetingTest() throws Exception {
+        mvc.perform(put("http://localhost:8080/hello-seatmap/greeting")
+            .content(greetingAsJSON(greetingString))
+            .contentType("application/json;charset=UTF-8"))
+            .andExpect(status().isOk());
+
+        mvc.perform(get("http://localhost:8080/hello-seatmap/greeting"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType("application/json;charset=UTF-8"))
+            .andExpect(content().string(equalTo(greetingAsJSON(greetingString))));
+    }
+
+    @Test
+    public void get404() throws Exception {
+        mvc.perform(get("http://localhost:8080/hello-seatmap/greeting"))
+            .andExpect(status().isNotFound());
+    }
+
+    private String greetingAsJSON(String greeting){
+        return "{\"greeting\":\""+greeting+"\"}";
+    }
+}
+*/
---
+++ src/test/java/HelloControllerTest.java
@@ -0,0 +1,24 @@
+/*
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MockServletContext.class)
+@WebAppConfiguration
+public class HelloControllerTest {
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception{
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+     public void helloTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().string(equalTo("Hello Seatmap")));
+    }
+
+}
+*/
